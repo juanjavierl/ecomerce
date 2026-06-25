4702a07acd706df3684cd65ab27ec2b451c6f2c7
javier <jjavierl.bo@gmail.com>
2026-06-24 22:42:01 -0400

    vercion inicial ecomerce

--- ventas/settings.py
+++ ventas/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f*5x^8-9#li5-vj4d4#zy51%h%7hh#1&9)@8@6a#-+ye1@d-!7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["vadje.com","www.vadje.com"]
 
 
 # Application definition
